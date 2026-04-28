77a8d338d13af43904dfb4e02463ec628264051f
Muhammad Abdullah <ak8757796@gmail.com>
2026-04-28 17:20:25 +0500

    made changes

--- src/app/components/PaymentMethod.tsx
+++ src/app/components/PaymentMethod.tsx
@@ -5,6 +5,8 @@ import { FaAngleDown } from "react-icons/fa";
 import { useCart } from "@/app/context/CartContext";
 import { useRouter } from "next/navigation";
 import { v4 as uuidv4 } from "uuid";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 interface FormData {
   firstname: string;
@@ -36,21 +38,31 @@ interface PaymentMethodProps {
   onStripePayment: (formData: FormData) => Promise<void>;
 }
 
-const PaymentMethod = ({ selectedOption, onStripePayment }: PaymentMethodProps) => {
+const PaymentMethod = ({
+  selectedOption,
+  onStripePayment,
+}: PaymentMethodProps) => {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
   } = useForm<FormData>();
+
   const { cartItems, clearCart } = useCart();
   const router = useRouter();
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session?.user?.email) {
+      setValue("email", session.user.email);
+    }
+  }, [session, setValue]);
 
   const submitHandler = async (data: FormData) => {
     if (selectedOption === "Stripe") {
-      
       await onStripePayment(data);
     } else {
-     
       const orderId = uuidv4().slice(0, 8);
       const orderDetails = {
         orderId,
@@ -62,7 +74,7 @@ const PaymentMethod = ({ selectedOption, onStripePayment }: PaymentMethodProps) 
         })),
         total: cartItems.reduce(
           (acc, item) => acc + parseFloat(String(item.price)) * item.quantity,
-          0
+          0,
         ),
         paymentMethod: selectedOption,
       };
@@ -175,14 +187,22 @@ const PaymentMethod = ({ selectedOption, onStripePayment }: PaymentMethodProps) 
           required
           pattern={/^[0-9]+$/}
         />
-        <InputField
-          label="Email"
-          id="email"
-          register={register}
-          errors={errors}
-          required
-          pattern={/^[^\s@]+@[^\s@]+\.[^\s@]+$/}
-        />
+        <div className="flex flex-col gap-[17px] w-full">
+          <label className="text-base font-semibold">Email</label>
+
+          <input
+            type="email"
+            value={session?.user?.email || ""}
+            readOnly
+            className="border border-[#9F9F9F] rounded-lg h-[70px] w-full text-base px-[14px] bg-gray-100 cursor-not-allowed"
+          />
+
+          <input
+            type="hidden"
+            {...register("email")}
+            value={session?.user?.email || ""}
+          />
+        </div>
       </form>
     </div>
   );
